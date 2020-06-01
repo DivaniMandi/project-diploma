726e7c930d43267d2d7669a31b5f8a4c2f5d0ae3
DivaniMandi <47211051+DivaniMandi@users.noreply.github.com>
2020-06-01 18:51:48 +0300

    Update config.js

--- Backend/functions/util/config.js
+++ Backend/functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyCbCvE0Zs7uw6DEPIOZZxLj96XNqZKsnas",
-    authDomain: "my-rec-app-715ea.firebaseapp.com",
-    databaseURL: "https://my-rec-app-715ea.firebaseio.com",
-    projectId: "my-rec-app-715ea",
-    storageBucket: "my-rec-app-715ea.appspot.com",
-    messagingSenderId: "601264623808",
-    appId: "1:601264623808:web:fc1d4523a17273b4005d25",
-    measurementId: "G-2N25HQBK89"
+    apiKey: "APIKEY",
+    authDomain: "AUTHDOMAIN",
+    databaseURL: "DBURL",
+    projectId: "PROJECTID",
+    storageBucket: "STORAGEBUCKET",
+    messagingSenderId: "MID",
+    appId: "APPID",
+    measurementId: "MEASUREMENTID"
 };
