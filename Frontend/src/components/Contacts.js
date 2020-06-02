faecf492f65f71806d46051a7647ff5de3fb1b3d
DivaniMandi <47211051+DivaniMandi@users.noreply.github.com>
2020-06-02 20:14:49 +0300

    Update Contacts.js

--- Frontend/src/components/Contacts.js
+++ Frontend/src/components/Contacts.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 
 import { sendEmail } from '../redux/actions/userActions';
@@ -38,48 +42,64 @@ class Contacts extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-         const emailDetails={
-            name:this.state.name,
-            userEmail:this.state.email,
-            message:this.state.message
+        const emailDetails = {
+            name: this.state.name,
+            userEmail: this.state.email,
+            message: this.state.message
         }
 
         this.props.sendEmail(emailDetails);
-        
+
     }
 
     render() {
 
-        //const { classes } = this.props;
+        const { 
+            user: { loading, authenticated } } = this.props;
+
+
+        let contactsMarkup = !loading ? (authenticated ? (
+            <div className="container" style={{ textAlign: 'center' }}>
+                <header>
+                    <h1>Contact us</h1>
+                </header>
+
+                <Form onSubmit={this.handleSubmit} style={{ width: '600px', paddingLeft: '50px', paddingTop: '50px' }}>
+                    <FormGroup>
+                        <Label for="name">Name:</Label>
+                        <Input type="text" name="name" id="name" onChange={this.handleChange} />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="email">Email:</Label>
+                        <Input type="email" name="email" id="email" placeholder="example@provider.com" onChange={this.handleChange} />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="message">Message:</Label>
+                        <Input type="textarea" name="message" id="message" onChange={this.handleChange} />
+                    </FormGroup>
+                    <Button variant="contained" color="primary">Send</Button>{'    '}
+                </Form>
+            </div>
+
+        ) :
+            (
+                <Paper style={{padding:20}}>
+                    <Typography variant="body2" align="center">
+                        Your session expired. Please sign in again
+                        </Typography>
+                    <div style={{textAlign:'center', margin: '20px 10px'}}>
+                        <Button variant="contained" color="primary" component={Link} to="/login">Sign in</Button>{'    '}
+                        <Button variant="contained" color="secondary" component={Link} to="/signup">Sign up</Button>
+                    </div>
+                </Paper>
+            )) : (<p>loading...</p>);
+
 
         return (
             <MuiThemeProvider>
-                <div className="container" style={{ textAlign: 'center' }}>
-                    <header>
-                        <h1>Contact us</h1>
-                    </header>
-
-                    <Form onSubmit={this.handleSubmit} style={{ width: '600px', paddingLeft: '50px', paddingTop: '50px' }}>
-                        <FormGroup>
-                            <Label for="name">Name:</Label>
-                            <Input type="text" name="name" id="name" onChange={this.handleChange} />
-                        </FormGroup>
-
-                        <FormGroup>
-                            <Label for="email">Email:</Label>
-                            <Input type="email" name="email" id="email" placeholder="example@provider.com" onChange={this.handleChange} />
-                        </FormGroup>
-
-                        <FormGroup>
-                            <Label for="message">Message:</Label>
-                            <Input type="textarea" name="message" id="message" onChange={this.handleChange} />
-                        </FormGroup>
-
-                        <Button color="primary">Send</Button>
-                    </Form>
-
-
-                </div>
+                {contactsMarkup}
             </MuiThemeProvider>
 
         );
